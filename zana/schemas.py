d0bdfda4d064edcb6fa26a9fa89f74767a0ed672
Sam Nicholls <samstudio8@gmail.com>
2021-03-25 14:03:42 +0000

    Move assignment properties from Base

--- zana/schemas.py
+++ zana/schemas.py
@@ -6,13 +6,15 @@ from pydantic import BaseModel
 
 class ZealIdentifierBase(BaseModel):
     zeal: str
-    is_assigned: Optional[bool] = False
-    assigned_to: Optional[str] = None
-    assigned_on: Optional[datetime.datetime] = None
 
 class ZealIdentifierCreate(ZealIdentifierBase):
     pass
 
 class ZealIdentifier(ZealIdentifierBase):
+
+    assigned_to: Optional[str] = None
+    is_assigned: Optional[bool] = False
+    assigned_on: Optional[datetime.datetime] = None
+
     class Config:
         orm_mode = True
